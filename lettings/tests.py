887ab798b41847863252e8e9addee31e8c7816ca
glen <glen84_dr@hotmail.com>
2025-08-12 01:11:50 +0200

    model creation tests

--- lettings/tests.py
+++ lettings/tests.py
@@ -1,3 +1,41 @@
 from django.test import TestCase
+from lettings.models import Address, Letting
 
-# Create your tests here.
+
+class AddressTestCase(TestCase):
+    def setUp(self):
+        """
+        Creating address objects
+        """
+        Address.objects.create(number=7, street="Bd de la Victoire",
+                               city="Strasbourg", state="France",
+                               zip_code=67000, country_iso_code="3166-2:FR")
+        Address.objects.create(number=1, street="Piazzale Loreto",
+                               city="Milano", state="Italy", zip_code=20127,
+                               country_iso_code="3166-1:alpha-2")
+
+    def test_address_creation(self):
+        """
+        Testing the datas of adresses created
+        """
+        ad1 = Address.objects.get(number=7)
+        ad2 = Address.objects.get(number=1)
+        self.assertEqual(ad1.street, "Bd de la Victoire")
+        self.assertEqual(ad1.city, "Strasbourg")
+        self.assertEqual(ad1.zip_code, 67000)
+        self.assertEqual(ad1.country_iso_code, "3166-2:FR")
+        self.assertEqual(ad2.street, "Piazzale Loreto")
+        self.assertEqual(ad2.zip_code, 20127)
+        self.assertEqual(ad2.state, "Italy")
+
+    def test_letting(self):
+        ad = Address.objects.get(number=1)
+        ad2 = Address.objects.get(number=7)
+        letting1 = Letting.objects.create(title="We hunged Mussolini here",
+                                          address=ad)
+        letting2 = Letting.objects.create(title="The Tram is still there",
+                                          address=ad2)
+        self.assertEqual(letting1.title, "We hunged Mussolini here")
+        self.assertEqual(letting1.address.city, "Milano")
+        self.assertEqual(letting2.title, "The Tram is still there")
+        self.assertEqual(letting2.address.country_iso_code, "3166-2:FR")
--- profiles/tests.py
+++ profiles/tests.py
@@ -1,3 +1,25 @@
 from django.test import TestCase
+from profiles.models import Profile
+from django.contrib.auth.models import User
 
-# Create your tests here.
+
+class ProfileTestCase(TestCase):
+    def setUp(self):
+        """
+        Creating provie objects
+        """
+        usr1 = User.objects.create(username="Anne", email="a@mail.fr")
+        usr2 = User.objects.create(username="Ben", email="b@mail.fr")
+        Profile.objects.create(favorite_city="St Denis", user=usr1)
+        Profile.objects.create(favorite_city="Strasbourg", user=usr2)
+
+    def test_profile_creation(self):
+        """
+        Testing the profile's data
+        """
+        p1 = Profile.objects.get(user__email="a@mail.fr")
+        p2 = Profile.objects.get(user__email="b@mail.fr")
+        self.assertEqual(p1.favorite_city, "St Denis")
+        self.assertEqual(p1.user.username, "Anne")
+        self.assertEqual(p2.favorite_city, "Strasbourg")
+        self.assertEqual(p2.user.username, "Ben")
