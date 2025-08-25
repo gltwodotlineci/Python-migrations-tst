c8685f9f1f73ae38e50c7fdcfe604bf20728cea6
glen <glen84_dr@hotmail.com>
2025-08-25 22:42:33 +0200

    addin allowed host

--- oc_lettings_site/settings.py
+++ oc_lettings_site/settings.py
@@ -28,10 +28,8 @@ SECRET_KEY = 'fp$9^593hsriajg$_%=5trot9g!1qa@ew(o-1#@=&4%=hp46(s'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [
-    "python-migrations-tst-4.onrender.com",
-    "localhost",
-]
+ALLOWED_HOSTS = ["*"]
+
 
 
 # Application definition
