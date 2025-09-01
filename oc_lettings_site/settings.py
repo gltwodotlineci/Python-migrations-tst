f8a87c7e07c99a3c9a932309fa0b9ab45e7ccb4c
glen <glen84_dr@hotmail.com>
2025-09-01 20:42:02 +0200

    correcting Debug in prod

--- oc_lettings_site/settings.py
+++ oc_lettings_site/settings.py
@@ -32,8 +32,8 @@ ALLOWED_HOSTS = ["*"]
 
 if os.getenv("SECRET_KEY"):
     SECRET_KEY = os.getenv("SECRET_KEY")
-    ALLOWED_HOSTS = ["*"]
-
+    ALLOWED_HOSTS = ["https://lettings-test.onrender.com/"]
+    DEBUG = False
 
 # Application definition
 
