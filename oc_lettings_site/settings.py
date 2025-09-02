508f61d20a4647742114ed82eed3d4b237bb65a6
glen <glen84_dr@hotmail.com>
2025-09-02 09:37:48 +0200

    test00 url

--- oc_lettings_site/settings.py
+++ oc_lettings_site/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'fp$9^593hsriajg$_%=5trot9g!1qa@ew(o-1#@=&4%=hp46(s'
 # SECRET_KEY = os.getenv("SECRET_KEY")
-DEBUG = False
+DEBUG = True
 ALLOWED_HOSTS = ["migrations-deployment-tst.onrender.com"]
 
 # if os.getenv("SECRET_KEY"):
