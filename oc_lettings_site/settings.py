3c56922af9c13655fa71ab58c5f09cbf8c021b80
glen <glen84_dr@hotmail.com>
2025-09-02 11:52:51 +0200

    test prod env064

--- oc_lettings_site/settings.py
+++ oc_lettings_site/settings.py
@@ -32,8 +32,8 @@ ALLOWED_HOSTS = ["wrong_url"]
 
 if os.getenv("SECRET_KEY"):
     SECRET_KEY = os.getenv("SECRET_KEY")
-    ALLOWED_HOSTS = ["migrations-deployment-tst.onrender.com"]
     DEBUG = True
+    ALLOWED_HOSTS = ["migrations-deployment-tst.onrender.com"]
 
 
 # LOGIN LOGIN TEST LOGIN TEST
