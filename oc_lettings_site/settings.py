6c269355f0f8050e5b8a6bdaa498209c888e7b7d
glen <glen84_dr@hotmail.com>
2025-09-02 02:19:15 +0200

    test prod env

--- oc_lettings_site/settings.py
+++ oc_lettings_site/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'fp$9^593hsriajg$_%=5trot9g!1qa@ew(o-1#@=&4%=hp46(s'
-DEBUG = True
+DEBUG = False
 ALLOWED_HOSTS = ["localhost"]
 
 if os.getenv("SECRET_KEY"):
