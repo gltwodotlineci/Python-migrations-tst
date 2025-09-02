04dccf37ab11164466d87bb6ca617a538780bbb9
glen <glen84_dr@hotmail.com>
2025-09-02 03:09:53 +0200

    test prod env01

--- oc_lettings_site/settings.py
+++ oc_lettings_site/settings.py
@@ -27,13 +27,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'fp$9^593hsriajg$_%=5trot9g!1qa@ew(o-1#@=&4%=hp46(s'
-DEBUG = False
-ALLOWED_HOSTS = ["*"]
+DEBUG = True
+ALLOWED_HOSTS = ["migrations-deployment-tst.onrender.com"]
 
-if os.getenv("SECRET_KEY"):
-    SECRET_KEY = os.getenv("SECRET_KEY")
-    ALLOWED_HOSTS = ["migrations-deployment-tst.onrender.com"]
-    DEBUG = False
+# if os.getenv("SECRET_KEY"):
+#     SECRET_KEY = os.getenv("SECRET_KEY")
+#     ALLOWED_HOSTS = ["migrations-deployment-tst.onrender.com"]
+#     DEBUG = False
 
 CSRF_TRUSTED_ORIGINS = [
     "https://lettings-test.onrender.com"
