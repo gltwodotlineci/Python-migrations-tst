fdb889930d7bf3de596fd1a4d4912558281798d4
glen <glen84_dr@hotmail.com>
2025-09-02 03:14:41 +0200

    test prod env02

--- oc_lettings_site/settings.py
+++ oc_lettings_site/settings.py
@@ -26,9 +26,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'fp$9^593hsriajg$_%=5trot9g!1qa@ew(o-1#@=&4%=hp46(s'
+# SECRET_KEY = 'fp$9^593hsriajg$_%=5trot9g!1qa@ew(o-1#@=&4%=hp46(s'
+SECRET_KEY = os.getenv("SECRET_KEY")
 DEBUG = True
-ALLOWED_HOSTS = ["migrations-deployment-tst.onrender.com"]
+ALLOWED_HOSTS = ["*"]
 
 # if os.getenv("SECRET_KEY"):
 #     SECRET_KEY = os.getenv("SECRET_KEY")
