9bc3582e5dd27d086637fc7ba486bd6a3b834780
glen <glen84_dr@hotmail.com>
2025-09-03 13:47:53 +0200

    test040

--- oc_lettings_site/settings.py
+++ oc_lettings_site/settings.py
@@ -26,14 +26,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'fp$9^593hsriajg$_%=5trot9g!1qa@ew(o-1#@=&4%=hp46(s'
-DEBUG = False
-ALLOWED_HOSTS = ["wrong_url"]
+DEBUG = True
+ALLOWED_HOSTS = ["*"]
 
 if os.getenv("SECRET_KEY"):
-    SECRET_KEY = os.getenv("SECRET_KEY")
-    # DEBUG = False
-    ALLOWED_HOSTS = ["lettings-test.onrender.com"]
-
+    DEBUG = os.getenv("DEBUG", "False").lower() in ("true", "1")
+    ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS",
+                              "lettings-test.onrender.com").split(",")
 
 # LOGGING in Prod
 LOGGING = {
