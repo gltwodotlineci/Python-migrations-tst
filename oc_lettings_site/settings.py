a1aa24f2873be8d84d18c29a72ba763e895cdfb2
glen <glen84_dr@hotmail.com>
2025-09-03 18:03:10 +0200

    fixing url

--- oc_lettings_site/settings.py
+++ oc_lettings_site/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 if ENVIRONMENT == "PRODUCTION":
     SECRET_KEY = os.getenv("SECRET_KEY")
     DEBUG = False
-    ALLOWED_HOSTS = ["lettings-test.onrender.com"]
+    ALLOWED_HOSTS = ["oc-lettings-site-docker.onrender.com"]
 else:
     SECRET_KEY = 'fp$9^593hsriajg$_%=5trot9g!1qa@ew(o-1#@=&4%=hp46(s'
     DEBUG = True
