9abec779ea7b114c4d84bbe56a83fea08e90b5c2
glen <glen84_dr@hotmail.com>
2025-09-03 10:29:47 +0200

    test wtn03

--- oc_lettings_site/settings.py
+++ oc_lettings_site/settings.py
@@ -3,6 +3,7 @@ from sentry_sdk.integrations.django import DjangoIntegration
 from pathlib import Path
 
 import os
+import sys
 import sentry_sdk
 
 
@@ -66,7 +67,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    # "whitenoise.middleware.WhiteNoiseMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -75,11 +76,20 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-STORAGES = {
-    "staticfiles": {
-        "BACKEND": "storages.backends.s3boto3.S3Boto3Storage",
+if "test" in sys.argv or os.environ.get("DJANGO_DEVELOPMENT", False):
+    # Local development & tests
+    STORAGES = {
+        "staticfiles": {
+            "BACKEND": "django.contrib.staticfiles.storage.StaticFilesStorage",
+        }
     }
-}
+else:
+    # Production (Render, Docker, etc.)
+    STORAGES = {
+        "staticfiles": {
+            "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+        }
+    }
 
 ROOT_URLCONF = 'oc_lettings_site.urls'
 
@@ -152,18 +162,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-STATICFILES_DIRS = [BASE_DIR / "static",]
+STATIC_URL = '/static/'
 
-# This is where collectstatic will collect all static files for production
+# Local static assets (your app-level static/ folders)
+STATICFILES_DIRS = [BASE_DIR / "static"]
+
+# Where collectstatic will put all static files for production
 STATIC_ROOT = BASE_DIR / "staticfiles"
-
-AWS_ACCESS_KEY_ID = os.getenv("AWS_ACCESS_KEY_ID")
-AWS_SECRET_ACCESS_KEY = os.getenv("AWS_SECRET_ACCESS_KEY")
-AWS_STORAGE_BUCKET_NAME = "my-static-files-cd-test-app"
-AWS_S3_REGION_NAME = "eu-west-3"
-AWS_QUERYSTRING_AUTH = False
-
-# S3 (Amazon Web Services) settings for static files
-STATICFILES_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
-# URL for static files
-STATIC_URL = f"https://{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com/"
--- oc_lettings_site/urls.py
+++ oc_lettings_site/urls.py
@@ -2,8 +2,8 @@ from django.contrib import admin
 from django.urls import path, include
 from . import views
 
-handler404 = 'oc_lettings_site.views.views_404'
-handler500 = 'oc_lettings_site.views.views_500'
+# handler404 = 'oc_lettings_site.views.views_404'
+# handler500 = 'oc_lettings_site.views.views_500'
 
 urlpatterns = [
     path('', include('lettings.urls')),
